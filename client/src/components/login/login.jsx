bae0bd50eff66400b24871fa0d1350c6257e612b
samriddhitiwary <151003533+samriddhitiwary@users.noreply.github.com>
2024-09-28 11:38:08 +0530

    Login

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -22,13 +22,13 @@ const Login = () => {
   }
 
   const submitForm = async (e) => {
-    // e.preventDefault();
-    // await axios.post("http://localhost:8000/api/getone/:id", user)
-    //   .then((response) => {
-    //     toast.success(response.data.msg, { position: "top-right" })
-    //     navigate("/")
-    //   })
-    //   .catch(error => console.log(error))
+    e.preventDefault();
+    await axios.post("http://localhost:8000/api/update:id", user)
+      .then((response) => {
+        toast.success(response.data.msg, { position: "top-right" })
+        navigate("/")
+      })
+      .catch(error => console.log(error))
   }
 
   return (
@@ -36,14 +36,31 @@ const Login = () => {
       <Link to={"/"}>Back</Link>
       <h1>USER LOGIN PORTAL</h1>
       <form className='addUserForm' onSubmit={submitForm}>
-       
+
+      <div className="inputGroup">
+          <label htmlFor="fname">First name</label>
+          <input type="text" onChange={inputHandler} id="fname" name="fname" autoComplete='off' placeholder='First name' />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="lname">Last name</label>
+          <input type="text" onChange={inputHandler} id="lname" name="lname" autoComplete='off' placeholder='Last name' />
+        </div>
         <div className="inputGroup">
           <label htmlFor="email">Email</label>
           <input type="email" onChange={inputHandler} id="email" name="email" autoComplete='off' placeholder='Email' />
         </div>
+     
         <div className="inputGroup">
           <label htmlFor="password">Password</label>
           <input type="password" onChange={inputHandler} id="password" name="password" autoComplete='off' placeholder='Password' />
+        </div>
+        <div className="inputGroup">
+          <label htmlFor="role">Role</label>
+          <select name="role" onChange={inputHandler} id="role">
+            <option value="Patients">Patients</option>
+            <option value="Doctors">Doctors</option>
+            <option value="Admin">Admin</option>
+          </select>
         </div>
       
         <div className="inputGroup">
